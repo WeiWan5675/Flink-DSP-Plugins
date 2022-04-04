c798e34bebedb22f733f0c460555fca7a3523dcb
xiaozhennan1995@gmail.com <xiaozhennan1995@gmail.com>
2022-04-04 20:44:38 +0800

    sync

--- dsp-plugin-kafka-connector/src/main/java/com/weiwan/dsp/kafka/connector/input/KafkaInputConnectorPlugin.java
+++ dsp-plugin-kafka-connector/src/main/java/com/weiwan/dsp/kafka/connector/input/KafkaInputConnectorPlugin.java
@@ -4,7 +4,6 @@ import com.weiwan.dsp.core.plugin.flink.ConnectorInputPlugin;
 import com.weiwan.dsp.core.pub.DataRecord;
 import com.weiwan.dsp.kafka.connector.config.KafkaConnectorInputPluginConfig;
 import com.weiwan.dsp.kafka.connector.kafka.KafkaDeserializationSchema;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
@@ -20,7 +19,7 @@ import java.util.Properties;
 public class KafkaInputConnectorPlugin extends ConnectorInputPlugin<KafkaConnectorInputPluginConfig> {
 
     @Override
-    public SourceFunction<DataRecord> getReaderSource(KafkaConnectorInputPluginConfig pluginConfig) {
+    public SourceFunction<DataRecord> getSourceFunction(KafkaConnectorInputPluginConfig pluginConfig) {
         Properties properties = pluginConfig.getKafkaProperties();
         String topic = pluginConfig.getTopic();
         FlinkKafkaConsumer kafkaConsumer = new FlinkKafkaConsumer(topic, new KafkaDeserializationSchema(), properties);
